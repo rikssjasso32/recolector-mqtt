d4ed5123932df9ec97dd88532f6bc52d55e1cb9a
Ricardo Jasso <rikssjasso.ca@gmail.com>
2026-04-20 17:58:16 -0600

    Guardar modo en config

--- recolector.js
+++ recolector.js
@@ -89,7 +89,7 @@ app.delete('/historial', (req, res) => {
 // 🔥 GUARDAR CONFIG
 app.post('/config', (req, res) => {
   try {
-    const { surco, planta, min, max } = req.body;
+    const { surco, planta, min, max, modo } = req.body;
 
     let data = [];
 
@@ -99,7 +99,7 @@ app.post('/config', (req, res) => {
 
     const index = data.findIndex(d => d.surco === surco);
 
-    const nuevaConfig = { surco, planta, min, max };
+    const nuevaConfig = { surco, planta, min, max, modo };
 
     if (index >= 0) {
       data[index] = nuevaConfig;
